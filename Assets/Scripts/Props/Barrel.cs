e66f35b8a954f5d58fcc0808f8d6893419d992e8
Vinicius <vncastanheira@gmail.com>
2017-10-10 23:56:08 -0300

    Explosion effects

--- Assets/Scripts/Props/Barrel.cs
+++ Assets/Scripts/Props/Barrel.cs
@@ -5,38 +5,55 @@ public class Barrel : MonoBehaviour
 {
 	public GameObject ExplosionFX;
 	public LayerMask destructibleLayer;
-	
+	public float radius;
+
+	public AudioSource explosionSFX;
+
 	void OnCollisionEnter(Collision collision)
 	{
 		if (collision.gameObject.CompareTag("Projectile"))
 		{
+			Debug.Log(name + " exploded by a projectile!");
 			Explode();
 		}
 	}
 
 	public void Explode()
 	{
+		var soundEffect = Instantiate(explosionSFX, transform.position, transform.rotation);
+		Destroy(soundEffect, 6f);
+
 		var explosion = Instantiate(ExplosionFX, transform.position - (Vector3.up * 2), ExplosionFX.transform.rotation);
 		Destroy(explosion.gameObject, 2f);
-		Destroy(gameObject);
-
+		
 		StartCoroutine(ExplodingNearby());
 	}
 
 	IEnumerator ExplodingNearby()
 	{
-		yield return new WaitForFixedUpdate();
-		foreach (var hit in Physics.SphereCastAll(transform.position, 150, Vector3.up, 50, destructibleLayer))
+		yield return new WaitForSecondsRealtime(0.2f);
+		foreach (var hit in Physics.SphereCastAll(transform.position, radius, Vector3.up, 50, destructibleLayer))
 		{
 			if (hit.collider.gameObject == gameObject)
+			{
+				Debug.Log(name + " hit itself!");
 				yield return null;
+			}
 
 			var barrel = hit.transform.GetComponent<Barrel>();
 			if (barrel != null)
 			{
+				Debug.Log(string.Format("{0} exploded {1}", name, barrel.gameObject.name));
 				barrel.Explode();
 			}
-			yield return null;
+
+			var destructible = hit.transform.GetComponent<DestructibleConcrete>();
+			if (destructible != null)
+			{
+				destructible.OnDestructibleDestroy();
+			}
 		}
+		Destroy(gameObject);
+		yield return null;
 	}
 }
---
+++ Assets/Scripts/Props/DestructibleConcrete.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class DestructibleConcrete : MonoBehaviour
+{
+	public ParticleSystem particles;
+
+	MeshRenderer m_Renderer;
+	MeshCollider m_Collider;
+	bool isDestroyed = false;
+
+	private void Start()
+	{
+		m_Renderer = GetComponent<MeshRenderer>();
+		m_Collider = GetComponent<MeshCollider>();
+	}
+
+	public void OnDestructibleDestroy()
+	{
+		if (!isDestroyed)
+		{
+			isDestroyed = true;
+			m_Renderer.enabled = false;
+			m_Collider.enabled = false;
+			Instantiate(particles, transform.position, particles.transform.rotation);
+			Destroy(gameObject, 3f);
+		}
+	}
+}
